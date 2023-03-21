6005df8c1f9d13c3addd84a7eb5a7938e47053fc
sharifgk <sharif_khan@live.co.uk>
2023-03-21 23:13:09 +0000

    navbar fixed

--- FrontEnd/chitter/src/Components/Navigation.jsx
+++ FrontEnd/chitter/src/Components/Navigation.jsx
@@ -3,10 +3,21 @@ import { NavLink } from 'react-router-dom';
 
 const Navigation = () => {
     return (
-        <nav className="navbar navbar-expand-1g navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container">
                 <NavLink className="navbar-brand" to="/">Chitter</NavLink>
-                <div className="collapse navbar-collapse">
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav ml-auto">
                         <li className="nav-item">
                             <NavLink className="nav-link" to="/login">Login</NavLink>
@@ -20,5 +31,6 @@ const Navigation = () => {
         </nav>
     );
 };
+
 
 export default Navigation;
