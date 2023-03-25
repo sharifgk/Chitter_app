a044142ecbbd7f6a28f2621cbc250d411dbe8d83
sharifgk <sharif_khan@live.co.uk>
2023-03-25 20:55:45 +0000

    sign up working FE+BE

--- FrontEnd/chitter/src/Components/SignUpForm.jsx
+++ FrontEnd/chitter/src/Components/SignUpForm.jsx
@@ -1,24 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import { signup } from "../Utils/api.js";
 
 const SignUpForm = () => {
-    return (
+    const [error, setError] = useState('');
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        const data = {
+            username: event.target.username.value,
+            email: event.target.email.value,
+            password: event.target.password.value,
+        };
+
+        try {
+            const response = await signup(data);
+            // Handle successful sign up response
+        } catch (error) {
+            // Handle error
+            if (error.response) {
+                setError(error.response.data.message);
+            } else {
+                setError('An error occurred while connecting to the server');
+            }
+        }
+    };
+   
+   return (
         <div className="container">
             <h2>Sign Up</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="form-group">
                     <label htmlFor="username">Username</label>
-                    <input type="text" className="form-control" id="username" />
+                    <input type="text" className="form-control" id="username" name="username" />
                 </div>
                 <div className="form-group">
                     <label htmlFor="email">Email address</label>
-                    <input type="email" className="form-control" id="email" />
+                    <input type="email" className="form-control" id="email" name="email" />
                 </div>
                 <div className="form-group">
                     <label htmlFor="password">Password</label>
-                    <input type="text" className="form-control" id="password" />
+                    <input type="text" className="form-control" id="password" name="password" />
                 </div>
                 <button type="submit" className="btn btn-primary">Sign Up</button>
-            </form>
+           </form>
+           {error && <div className="alert alert-danger mt-3">{error}</div>}
         </div>
     );
 };
---
+++ FrontEnd/chitter/src/Utils/api.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+// const api = axios.create({
+//     baseURL: 'http://localhost:5000/api/',
+// });
+
+const URL = 'http://localhost:5000/api/users/'
+
+// export const signup = (data) => api.post('/signup', data);
+// export const login = (data) => api.post('/login', data);
+
+export const signup = async (data) => {
+    try {
+        console.log(data);
+        const response = await axios.post(URL + 'signup', data);
+        return response.data
+    } catch (e) {
+        return { error: e.code, errorMessage: e.message };
+    }
+};
